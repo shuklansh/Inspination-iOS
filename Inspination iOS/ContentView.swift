4bfb3940b4a3cb42ad01902b64937bc284ab1538
shuklansh <ansh.shukla270501@gmail.com>
2024-09-07 14:33:46 +0530

    Initial Commit

---
+++ Inspination iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Inspination iOS
+//
+//  Created by Ansh Shukla on 07/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Inspination iOS/Inspination_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inspination_iOSApp.swift
+//  Inspination iOS
+//
+//  Created by Ansh Shukla on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Inspination_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
